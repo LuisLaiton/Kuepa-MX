2276b3f0b988633949b4c62ba84f67c587e40ed4
LuisLaiton <50175115+LuisLaiton@users.noreply.github.com>
2023-07-24 22:41:06 -0500

    Contador usando variable reactiva

---
+++ Semana-1/cont-ref.vue
@@ -0,0 +1,23 @@
+<!-- 
+  Crear un contador donde cada vez que el usuario precione el boton el numero aumentara
+-->
+<script setup>
+// Importar la función "ref" de Vue.js
+import { ref } from 'vue'
+
+// Declarar una variable reactiva llamada "num" con valor inicial 0
+let num = ref(0)
+
+// Definir la función "incrementar" que aumenta en 1 el valor de la variable "num" y muestra el nuevo valor en la consola
+let incrementar = () => {
+  num.value++
+  console.log(num.value)
+}
+</script>
+
+<template>
+  <!-- Mostrar el valor actual de la variable "num" en un título -->
+  <h1>{{ num }}</h1>
+  <!-- Crear un botón que, al hacer clic, ejecuta la función "incrementar" -->
+  <button @click="incrementar">Aumentar</button>
+</template>
