ae68625d86bb8d637a8fef38d3e64437f0304379
LuisLaiton <50175115+LuisLaiton@users.noreply.github.com>
2023-07-24 22:39:44 -0500

    Usando v-for con un arreglo de objetos

---
+++ Semana-1/arreglo-objetos.vue
@@ -0,0 +1,37 @@
+<!-- 
+  Mostrar de forma ordenada el arreglo de objetos mencionado 
+-->
+
+<script setup>
+// Declaración y definición de la variable "users" como un arreglo de objetos que representan usuarios
+let users = [
+  {
+    user: 'Luis',
+    password: '123',
+    edad: 45
+  },
+  {
+    user: 'Felipe',
+    password: '456',
+    edad: 54
+  },
+  {
+    user: 'Laiton',
+    password: '789',
+    edad: 32
+  }
+]
+</script>
+
+<template>
+  <!-- Mostrar el arreglo completo de objetos "users" -->
+  <p>{{ users }}</p>
+
+  <!-- Crear una lista que muestra la información de cada usuario -->
+  <ul>
+    <li v-for="(item, index) in users" :key="item.user">
+      <!-- Mostrar el nombre de usuario, contraseña y edad del usuario actual -->
+      {{ item.user }} - {{ item.password }} - {{ item.edad }}
+    </li>
+  </ul>
+</template>
