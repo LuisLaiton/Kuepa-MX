2baae17be2a143f5d9dc0d8c90af0615139da722
LuisLaiton <50175115+LuisLaiton@users.noreply.github.com>
2023-07-30 21:27:09 -0500

    Usando props para la informacion de cards

---
+++ componentes-cards/src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import Cards from './components/Cards.vue';
+
+const objCards = [
+  {
+    titulo: 'Card 1',
+    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-primary',
+    color_text: 'text-primary'
+  },
+  {
+    titulo: 'Card 2',
+    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-success',
+    color_text: 'text-success'
+  },
+  {
+    titulo: 'Card 3',
+    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-danger',
+    color_text: 'text-danger'
+  },
+  {
+    titulo: 'Card 4',
+    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-warning'
+  },
+  {
+    titulo: 'Card 5',
+    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-info',
+    color_text: 'text-info'
+  },
+  {
+    titulo: 'Card 6',
+    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'bg-dark',
+    color_text: 'text-light'
+  }
+]
+</script>
+
+<template>
+  <h1>Hola mundo</h1>
+  <section class="row row-cols-1 row-cols-md-3 g-4">
+    <Cards v-for="(item, index) in objCards" :key="index" :titulo="item.titulo" :texto="item.texto" :imagen="item.imagen" :color_card="item.color_card" :color_text="item.color_text" />
+  </section>
+</template>
---
+++ componentes-cards/src/components/Cards.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    titulo: String,
+    texto: String,
+    imagen: String,
+    color_card: String,
+    color_text:{
+        type: String,
+        default: 'text-dark'
+    }
+})
+</script>
+
+<template>
+    <div class="col">
+        <div class="card" :class="color_card">
+            <img :src="imagen" class="card-img-top" :alt="titulo">
+            <div class="card-body" :class="color_text">
+                <h5 class="card-title">{{ titulo }}</h5>
+                <p class="card-text">{{ texto }}</p>
+            </div>
+        </div>
+    </div>
+</template>
