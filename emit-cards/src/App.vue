394f03915c96ffe2933fa0c6ace109c087ad4216
LuisLaiton <50175115+LuisLaiton@users.noreply.github.com>
2023-07-30 21:27:55 -0500

    Enviado eventos a los componentes

---
+++ emit-cards/src/App.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { ref } from 'vue';
+import Cards from './components/Cards.vue';
+
+const objCards = [
+  {
+    producto: 'Card 1',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-primary',
+    color_text: 'text-primary'
+  },
+  {
+    producto: 'Card 2',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-success',
+    color_text: 'text-success'
+  },
+  {
+    producto: 'Card 3',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-danger',
+    color_text: 'text-danger'
+  },
+  {
+    producto: 'Card 4',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-warning'
+  },
+  {
+    producto: 'Card 5',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'border-info',
+    color_text: 'text-info'
+  },
+  {
+    producto: 'Card 6',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus vero velit similique quisquam, necessitatibus hic repellendus temporibus qui quaerat minima cumque asperiores nam reprehenderit ipsam. Aliquam iusto veniam nobis?',
+    imagen: 'https://placehold.co/600x400',
+    color_card: 'bg-dark',
+    color_text: 'text-light'
+  }
+]
+const favoritos = ref([])
+const añadirFavorito = (producto) => {
+  if(!favoritos.value.includes(producto)) {
+    favoritos.value.push(producto)
+  } else {
+    alert('El producto ya se encuentra en la lista')
+  }
+}
+</script>
+
+<template>
+  <h3>Productos favoritos</h3>
+  <ul class="list-group mx-auto my-5 col-6">
+    <li class="list-group-item" v-for="(item, index) in favoritos">
+      {{ item }}
+    </li>
+  </ul>
+  <section class="row row-cols-1 row-cols-md-3 g-4">
+    <Cards 
+      v-for="(item, index) in objCards"
+      :key="index"
+      :producto="item.producto"
+      :descripcion="item.descripcion"
+      :imagen="item.imagen"
+      :color_card="item.color_card"
+      :color_text="item.color_text"
+      @añadirFavorito="añadirFavorito"
+    />
+  </section>
+</template>
---
+++ emit-cards/src/components/Cards.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+    producto: String,
+    descripcion: String,
+    imagen: String,
+    color_card: String,
+    color_text: {
+        type: String,
+        default: 'text-dark'
+    }
+})
+
+const emit = defineEmits(['añadirFavorito'])
+</script>
+
+<template>
+    <div class="col">
+        <div class="card" :class="color_card">
+            <img :src="imagen" class="card-img-top" :alt="producto">
+            <div class="card-body" :class="color_text">
+                <h5 class="card-title">{{ producto }}</h5>
+                <p class="card-text">{{ descripcion }}</p>
+            </div>
+            <div class="d-grid">
+                <button @click="emit('añadirFavorito', producto)" class="btn btn-outline-primary" type="button">
+                    Añadir a favoritos
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
