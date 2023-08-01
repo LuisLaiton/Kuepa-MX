4dd1fc922212ee24c3296764322d32d9797c7df6
LuisLaiton <50175115+LuisLaiton@users.noreply.github.com>
2023-07-31 19:07:26 -0500

    Vue router

---
+++ router-1/src/components/ElEjemplo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hola mundo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim exercitationem, facere veritatis voluptates quam
+            necessitatibus cupiditate nam similique, mollitia porro commodi a dolorem quos nemo quia, possimus minima
+            provident nisi?
+            Unde rem, saepe distinctio id rerum suscipit pariatur tenetur architecto dolorum consectetur repudiandae
+            blanditiis officiis alias accusantium, cupiditate nam molestiae sunt tempore ipsa atque molestias. Tenetur vero
+            fuga odit quasi?
+        Modi voluptas, voluptatibus illo qui reprehenderit quia id cumque. Veniam neque impedit molestias ratione
+        numquam id quo officiis aut aliquam esse aperiam incidunt, laborum porro minus dolore nemo. Nam, similique.</p>
+        </div>
+</template>
---
+++ router-1/src/views/EjemploView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ElEjemplo from '../components/ElEjemplo.vue';
+</script>
+
+<template>
+   <ElEjemplo />
+</template>
